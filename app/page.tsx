daecd3eefc55210577de51136ab106bc39e8b8f2
22JH <toitoii080@gmail.com>
2024-07-14 11:31:28 +0900

    add sign

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,26 @@ export default function Home() {
   const test = async () => {
     console.log("1");
     const ethereum = sdk.getProvider();
-    const accounts = await ethereum?.request({ method: "eth_requestAccounts" });
-    console.log(accounts);
+    if (!ethereum) return;
+    const [accounts, chainId] = (await Promise.all([
+      ethereum.request({
+        method: "eth_requestAccounts",
+      }),
+      ethereum.request({
+        method: "eth_chainId",
+      }),
+    ])) as [string[], string];
+
+    if (!accounts) throw "There's no account";
+    // 지갑 목록을 가져옴, 현재 선택한 지갑이 0번째 인덱스
+    const walletAddress = accounts[0];
+
+    const signature = (await ethereum.request({
+      method: "personal_sign",
+      params: ["hello test", walletAddress],
+    })) as string;
+
+    console.log(signature, chainId);
   };
 
   const sdk = new MetaMaskSDK({
