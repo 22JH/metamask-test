6a01f82c414b96879f9011b2613befc637a8cec6
22JH <toitoii080@gmail.com>
2024-11-05 20:00:03 +0900

    delete metamask

--- app/page.tsx
+++ app/page.tsx
@@ -1,85 +1,11 @@
 "use client";
 
-import sdk2 from "@web3-onboard/metamask";
-import Onboard from "@web3-onboard/core";
-
-//@ts-ignore
-import MetaMaskSDK from "@metamask/sdk";
 import Link from "next/link";
 
 export default function Home() {
-  const metamaskSDKWallet = sdk2({
-    options: {
-      extensionOnly: false,
-      dappMetadata: {
-        name: "Example Web3-Onboard Dapp",
-      },
-    },
-  });
 
-  const test = async () => {
-    const ethereum = sdk.getProvider();
-
-    if (!ethereum) return;
-    await sdk.init();
-    await ethereum.request({
-      method: "wallet_addEthereumChain",
-      params: [
-        {
-          chainId: "0x2019",
-          chainName: "Klaytn Mainnet",
-          blockExplorerUrls: ["https://klaytnscope.com"],
-          nativeCurrency: { symbol: "KLAY", decimals: 18 },
-          rpcUrls: ["https://public-en-cypress.klaytn.net"],
-        },
-      ],
-    });
-    const [accounts, chainId] = (await Promise.all([
-      ethereum.request({
-        method: "eth_requestAccounts",
-      }),
-      ethereum.request({
-        method: "eth_chainId",
-      }),
-    ])) as [string[], string];
-
-    if (!accounts) throw "There's no account";
-    // 지갑 목록을 가져옴, 현재 선택한 지갑이 0번째 인덱스
-    const walletAddress = accounts[0];
-
-    const signature = (await ethereum.request({
-      method: "personal_sign",
-      params: ["hello test", walletAddress],
-    })) as string;
-
-    console.log(signature, chainId);
-  };
-
-  const sdk = new MetaMaskSDK({
-    dappMetadata: {
-      name: "test",
-      url: "https://22JH.github.io/metamask-test",
-    },
-  });
-
-  const test2 = async () => {
-    const ethereumSepolia = {
-      id: 11155111,
-      token: "ETH",
-      label: "Sepolia",
-      rpcUrl: "https://rpc.sepolia.org/",
-    };
-    const onboard = Onboard({
-      wallets: [metamaskSDKWallet],
-      chains: [ethereumSepolia],
-    });
-
-    const connectedWallets = await onboard.connectWallet();
-  };
   return (
     <>
-      <button onClick={test}>test</button>
-      <button onClick={test2}>test2</button>
       <Link href="test2">testLink</Link>
     </>
   );
--- app/test2/page.tsx
+++ app/test2/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //@ts-ignore
+import CircularJSON from 'circular-json';
 import { createWalletClient, custom } from 'viem';
 
 declare global {
@@ -10,15 +11,14 @@ declare global {
     Caver?: any;
     caver?: any;
     gfProvider?: any;
+    ethereum?: any;
   }
 }
 
 export default function test2() {
   const [account, setAccount] = useState<string | null>(null)
-
-  const walletClient = createWalletClient({
-    transport: custom(window.ethereum!),
-  })
+  const [klaytnInfo, setKlaytnInfo] = useState<string>("")
+  const [walletClient, setWalletClient] = useState<any>(null)
 
   const connect = async () => {
     const a = await window.klaytn.enable()
@@ -45,7 +45,20 @@ export default function test2() {
       alert(err)
     }
   };
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+
+    if (window.ethereum) {
+      setWalletClient(createWalletClient({
+        transport: custom(window.ethereum),
+      }))
+    }
+    setKlaytnInfo(CircularJSON.stringify(window.klaytn, null, 2))
+    
+  }, [])
   return <><button onClick={connect}>connect</button><button onClick={sign}>sign</button>
         <button onClick={wagmiSign}>wagmiSign</button>
+        <pre>{klaytnInfo}</pre>
   <button onClick={() => alert(window?.klaytn?.request)}>klaytn.requset</button></>;
 }
