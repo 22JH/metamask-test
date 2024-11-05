b7e7840f1bade1bab13b8ef2cadb0ac4d13113d2
22JH <toitoii080@gmail.com>
2024-11-05 17:23:20 +0900

    add type safe

--- app/test2/page.tsx
+++ app/test2/page.tsx
@@ -3,5 +3,5 @@
 import Link from "next/link";
 
 export default function test2() {
-  return <Link href="/">{Object.entries(window).map(([key, value]) => <div key={key}>{key}</div>)}</Link>;
+  return <Link href="/">{typeof window !== "undefined" && Object.entries(window).map(([key, value]) => <div key={key}>{key}</div>)}</Link>;
 }
